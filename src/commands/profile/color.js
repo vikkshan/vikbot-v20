159a0d7fcd4a8798407521c5dd28c0945c713afc
Vikshan <103361575+prince-vikshan@users.noreply.github.com>
2022-10-29 13:25:30 +0000

    added link to https://htmlcolorcodes.com/color-names/ when user does not define hex color code

--- src/commands/profile/color.js
+++ src/commands/profile/color.js
@@ -7,7 +7,7 @@ module.exports = async (client, interaction, args) => {
 
     Schema.findOne({ User: interaction.user.id }, async (err, data) => {
         if (data) {
-            if (!isHexcolor(color)) return client.errNormal({ error: "You did not specify an hex color! Example: #ff0000", type: 'editreply' }, interaction);
+            if (!isHexcolor(color)) return client.errNormal({ error: "You did not specify an hex color! Example: #ff0000. [Click here](https://htmlcolorcodes.com/color-names/) to see a list of all hex colors", type: 'editreply' }, interaction);
 
             data.Color = color;
             data.save();
