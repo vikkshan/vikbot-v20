874f2032e1785506c5b3e8a7593e0ed60c307994
Vikshan <103361575+prince-vikshan@users.noreply.github.com>
2022-10-29 20:03:34 +0000

    Fixed changelog

--- src/commands/bot/changelogs.js
+++ src/commands/bot/changelogs.js
@@ -3,13 +3,17 @@ const Discord = require('discord.js');
 module.exports = async (client, interaction, args) => {
     client.embed({
         title: "📃・Changelogs",
-        desc: `_____`,
+        desc: `I am your free friendly neighborhood bot with which you can run your entire server! With no less than 400+ commands. I would be glad if you helped me stay up by [buying me a coffee](https://ko-fi.com/vikbot)!`,
         thumbnail: client.user.avatarURL({ size: 1024 }),
         fields: [{
-                name: "📢┆Alert!",
-                value: 'After more than 1 year we decided to stop Bot on April 15th, for more information go to [this server](https://discord.gg/techpoint)',
+                name: "🐛┆Fixed Bugs!",
+                value: 'Better music system\nBetter bot setups\nGiveaways fixed\nChannel logs bug fixed\nBetter error handling for commands',
                 inline: false,
             },
+                 {
+                 name: "🍥┆Features!",
+                   value: 'Bot completely in slash commands\nNew Activities\nNew server stats\nAuto setups\nA new invite tracker system\nScheduled Events logs\nMute command is now timeout command\nEmbed layout improvements\nAn advanced embed builder\nRemoved NSFW commands\nUnnecessary commands removed\nSome commands rearranged'
+                 },
         ],
         type: 'editreply'
     }, interaction)
